fede16eac0fe6647618df90994ee9861e16c6176
Tanmay <tanmaysharma763@gmail.com>
2025-05-31 18:23:18 +0530

    dadw1

--- app/resume-analyzer/page.tsx
+++ app/resume-analyzer/page.tsx
@@ -4,8 +4,7 @@ import DisplayResume from '@/components/DisplayResume'
 const page = () => {
   return (
     <div className='w-full h-screen'>
-      All the resumes will be listed here.
-      <p>Click on a resume to analyze it.</p>
+      
       <ResumeUpload/>
       <DisplayResume/>
     </div>
